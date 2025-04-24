82d7a54a41cc69d399428291820327ddefbc0d3a
MuhammadRafay1 <rafaymuhammad71@gmail.com>
2025-04-24 22:05:54 +0500

    past/rebook feature added

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const {
+    getPastBookings,
     createBooking,
     getAllBookings,
     getUserBookings,
     getBookingById,
     updateBookingStatus,
-    deleteBooking
+    deleteBooking,
+
+    rebookProperty
 } = require("../controllers/bookingController");
 
 const { protect, admin } = require("../middlewares/authMiddlewares");
@@ -17,9 +20,15 @@ router.post("/", protect, createBooking);
 router.get("/", protect, admin, getAllBookings);
 router.get("/user/:userId", protect, getUserBookings);
 
-router.get("/:id", protect, getBookingById);
+
 router.patch("/:id", protect, admin, updateBookingStatus);
 
 router.delete("/:id", protect, deleteBooking);
 
+// Route to fetch past bookings
+router.get("/past", protect, getPastBookings);
+
+// Route to rebook a property
+router.post("/rebook", protect, rebookProperty);
+router.get("/:id", protect, getBookingById);
 module.exports = router;
